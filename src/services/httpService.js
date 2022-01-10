f886cf763b9b90c84ae35465e624522253e8bc35
r-ILIASS <iliass.root@gmail.com>
2022-01-10 14:15:12 +0100

    feat(httpservice): handle unexpected errors using axios interceptors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,22 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import config from "../config.json";
 
 export const apiUrl = config.apiEndpoint;
+
+axios.interceptors.response.use(null, (error) => {
+  const expected =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expected) {
+    console.error(error);
+    toast.error("An unexpected error occurred.", {
+      autoClose: 5000,
+    });
+  }
+});
 
 function setJwt(jwt) {
   axios.defaults.headers.common["x-auth-token"] = jwt;
